996069e9b32926f30443b5a3cb9dcd03d9bc34e8
Draco Apollyon <dapollyon@gmail.com>
2020-06-25 11:42:18 +0800

    - remove opt

--- detect-mss.py
+++ detect-mss.py
@@ -1,20 +1,18 @@
 import argparse
 
 import mss
-import numpy as np
 from utils.datasets import *
 from utils.utils import *
 
 
-def detect(save_img=False):
-    out, source, weights, view_img, save_txt, imgsz = \
-        opt.output, opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size
+def detect():
+    weights, imgsz = 'weights/yolov5s.pt', 640
+    agnostic_nms, augment = False, False
+    conf_thres, iou_thres = 0.4, 0.5
+    classes = None
 
     # Initialize
-    device = torch_utils.select_device(opt.device)
-    if os.path.exists(out):
-        shutil.rmtree(out)  # delete output folder
-    os.makedirs(out)  # make new output folder
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     half = device.type != 'cpu'  # half precision only supported on CUDA
 
     # Load model
@@ -23,22 +21,13 @@ def detect(save_img=False):
     if half:
         model.half()  # to FP16
 
-    # Set Dataloader
-    save_img = True
-
-    # Run inference
-    img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
-    _ = model(img.half() if half else img) if device.type != 'cpu' else None  # run once
-
-    path = "inference\\images\\bus.jpg"
     # Part of the screen to capture
     monitor = {"top": 40, "left": 0, "width": 800, "height": 640}
     with mss.mss() as sct:
-        sc = sct.grab(monitor) # BGRA
-        im0s = np.array(sc)[:, :, :3]
-        im0s = cv2.cvtColor(im0s, cv2.COLOR_BGRA2BGR)
-
-        cv2.rectangle(im0s, (10, 10), (50, 50), (255, 0, 0), thickness=3, lineType=cv2.LINE_AA)
+        sc = sct.grab(monitor)  # BGRA
+        # im0s = np.array(sc)[:, :, :3] # convert to BGR
+        im0s = cv2.cvtColor(np.array(sc),
+                            cv2.COLOR_BGRA2BGR)  # using cv2 to convert, so that the cv2.rectangle will not complaint
 
         img = letterbox(im0s, new_shape=imgsz)[0]  # resize image (keep ratio)
         img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
@@ -51,53 +40,27 @@ def detect(save_img=False):
             img = img.unsqueeze(0)
 
         # Inference
-        pred = model(img, augment=opt.augment)[0]
-
+        pred = model(img, augment=augment)[0]
         # Apply NMS
-        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+        pred = non_max_suppression(pred, conf_thres, iou_thres, classes=classes, agnostic=agnostic_nms)
 
         # Process detections
         for i, det in enumerate(pred):  # detections per image
-            p, s, im0 = path, '', im0s
-
-            save_path = str(Path(out) / Path(p).name)
             if det is not None and len(det):
                 # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0s.shape).round()
 
                 # Write results
                 for *xyxy, conf, cls in det:
                     label = '%s %.2f' % (int(cls), conf)
-                    # TODO - move function into there
-                    plot_one_box(xyxy, im0, label=label, color=int(cls), line_thickness=3)
+                    plot_one_box(xyxy, im0s, label=label, color=int(cls), line_thickness=3)
 
-            # Save results (image with detections)
-            cv2.imwrite(save_path, im0)
-            cv2.imshow('My Image', im0)
+            # Show result
+            cv2.imshow('My Image', im0s)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
 
-    print('Done')
-
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', type=str, default='weights/yolov5s.pt', help='model.pt path')
-    parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
-    parser.add_argument('--output', type=str, default='inference/output', help='output folder')  # output folder
-    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.4, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.5, help='IOU threshold for NMS')
-    parser.add_argument('--fourcc', type=str, default='mp4v', help='output video codec (verify ffmpeg support)')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true', help='display results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    opt = parser.parse_args()
-    opt.img_size = check_img_size(opt.img_size)
-    print(opt)
-
     with torch.no_grad():
         detect()
